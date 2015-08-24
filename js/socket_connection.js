e217e04ac3a145a4fde94991ccaa36dc1f50759c
David Fisher <fisherds@rose-hulman.edu>
2015-08-24 09:55:52 -0400

    remove use of Window. when not required

--- js/socket_connection.js
+++ js/socket_connection.js
@@ -14,11 +14,11 @@ function motorControl() {
   var firstIndex = formdata.indexOf('=');
   var secIndex = formdata.indexOf('&');
   var left_command = formdata.slice(firstIndex + 1, secIndex);
-  console.log('left_command: ' + left_command);
+  //console.log('left_command: ' + left_command);
 
   var thirdIndex = formdata.lastIndexOf('=');
   var right_command = formdata.slice(thirdIndex + 1, formdata.length);
-  console.log('right_command: ' + right_command);
+  //console.log('right_command: ' + right_command);
 
   var speedLeft = leftSpeed;
   if (!speedLeft) {
@@ -26,7 +26,6 @@ function motorControl() {
   }
   speedLeft = Math.floor(speedLeft);
   speedLeft = speedLeft.toString();
-  console.log(speedLeft);
 
   var speedRight = rightSpeed;
   if (!speedRight) {
@@ -34,22 +33,21 @@ function motorControl() {
   }
   speedRight = Math.floor(speedRight);
   speedRight = speedRight.toString();
-  console.log(speedRight);
 
   var msg = JSON.stringify({
     "command": "motors", "left_command": left_command, "right_command": right_command,
     "left_speed": speedLeft, "right_speed": speedRight
   });
-  window.socket.send(msg);
+  socket.send(msg);
 }
 
 $(document).ready(function () {
-  window.socket = new WebSocket("ws://127.0.0.1:9000");
+  socket = new WebSocket("ws://127.0.0.1:9000");
   socket.onopen = function () {
     var msg = JSON.stringify({
       "command": "ready"
     });
-    window.socket.send(msg);
+    socket.send(msg);
     console.log("Connected!");
     isopen = true;
   };
@@ -62,21 +60,21 @@ $(document).ready(function () {
       var xx = msg['x'];
       var yy = msg['y'];
       var zz = msg['z'];
-      Window.vprogressx.value = xx;
-      Window.vprogressy.value = yy;
-      Window.vprogressz.value = zz;
-      Window.vprogressx.draw();
-      Window.vprogressy.draw();
-      Window.vprogressz.draw();
+      vprogressx.value = xx;
+      vprogressy.value = yy;
+      vprogressz.value = zz;
+      vprogressx.draw();
+      vprogressy.draw();
+      vprogressz.draw();
       break;
       case 'encoders':
       var left = msg['left'];
       var right = msg['right'];
       // console.log('left = ' + left + 'right = ' + right);
-      Window.gauge4.value = right;
-      Window.gauge4.draw();
-      Window.gauge2.value = left;
-      Window.gauge2.draw();
+      gauge4.value = right;
+      gauge4.draw();
+      gauge2.value = left;
+      gauge2.draw();
       break;
       case 'ir1':
       val = msg['data'];

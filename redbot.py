134895b6731bc9c9a3c3a246d697552434defb9f
Alan Yorinks <MisterYsLab@gmail.com>
2015-10-21 15:43:05 -0400

    Accelerometer Updates

--- redbot.py
+++ redbot.py
@@ -103,8 +103,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-comport", dest="com", default="None", help="Arduino COM port")
     parser.add_argument("-wait", dest="wait", default="2", help="Arduino wait time")
-    parser.add_argument("-ipAddr", dest="ip_addr", default="None", help="Arduino IP Address (WiFly")
-    parser.add_argument("-ipPort", dest="ip_port", default="2000", help="Arduino IP port (WiFly")
+    parser.add_argument("-ipAddr", dest="ip_addr", default="None", help="Arduino IP Address (WiFly)")
+    parser.add_argument("-ipPort", dest="ip_port", default="2000", help="Arduino IP port (WiFly)")
     parser.add_argument("-handshake", dest="handshake", default="*HELLO*", help="IP Device Handshake String")
 
     args = parser.parse_args()
